4089d94476a06b689ea2bb59e1040d8812c162ff
anthony-dee <anthonydouglas525@gmail.com>
2022-04-14 20:41:32 +0100

    handle situation where pluckKey is null

--- src/Macros/PluckThenGroupBy.php
+++ src/Macros/PluckThenGroupBy.php
@@ -5,27 +5,37 @@ namespace AnthonyDee\CollectionMacros\Macros;
 use Illuminate\Support\Collection;
 
 /**
- * @mixin \Illuminate\Support\Collection
+ * @mixin Collection
  */
 class PluckThenGroupBy {
     public function __invoke(): \Closure
     {
-        return function ($groupBy, $pluckValue, $pluckKey) {
+        return function ($groupBy, $pluckValue, $pluckKey = null) {
             $groupNameRetriever = $this->valueRetriever($groupBy);
             $pluckValueRetriever = $this->valueRetriever($pluckValue);
-            $pluckKeyRetriever = $this->valueRetriever($pluckKey);
+
+            if (!is_null($pluckKey)) {
+                $pluckKeyRetriever = $this->valueRetriever($pluckKey);
+            }
 
             $results = new Collection();
 
-            foreach($this->items as $key => $value) {
-                $groupName = $groupNameRetriever($value);
-                $pluckedVal = $pluckValueRetriever($value);
-                $pluckedKey = $pluckKeyRetriever($value);
+            foreach($this->items as $item) {
+                $groupName = $groupNameRetriever($item);
+                $pluckedVal = $pluckValueRetriever($item);
+
+                if (!is_null($pluckKey)) {
+                    $pluckedKey = $pluckKeyRetriever($item);
+                }
 
                 if (!$results->has($groupName)) {
-                    $results->offsetSet($groupName, new Collection([$pluckedKey => $pluckedVal]));
+                    $results->offsetSet($groupName, new Collection());
+                }
+
+                if (!is_null($pluckKey)) {
+                    $results->get($groupName)->put($pluckedKey, $pluckedVal);
                 } else {
-                    $results->get($groupName)->offsetSet($pluckedKey, $pluckedVal);
+                    $results->get($groupName)->push($pluckedVal);
                 }
             }
 
